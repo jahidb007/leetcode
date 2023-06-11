93a06e0e9c716177f8182445d326d7fa23f8a429
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-06-12 00:02:47 +0600

    Time: 4 ms (95.00%) | Memory: 43.5 MB (43.23%) - LeetSync

--- roman-to-integer/roman-to-integer.java
+++ roman-to-integer/roman-to-integer.java
@@ -1,57 +1,43 @@
 class Solution {
     public int romanToInt(String s) {
-         int i = 0;
-        String val;
+       int i = s.length() - 1;
+        int num = 0;
+        int prev = 0;
         int sum = 0;
-        while (i<s.length()){
-            val = String.valueOf(s.charAt(i));
 
-            if(i<s.length()-1){
-                val = val + String.valueOf(s.charAt(i+1));
+        while (i >= 0) {
+
+            switch (s.charAt(i)) {
+                case 'I':
+                    num = 1;
+                    break;
+                case 'V':
+                    num = 5;
+                    break;
+                case 'X':
+                    num = 10;
+                    break;
+                case 'L':
+                    num = 50;
+                    break;
+                case 'C':
+                    num = 100;
+                    break;
+                case 'D':
+                    num = 500;
+                    break;
+                case 'M':
+                    num = 1000;
+                    break;
             }
-
-            if(val.equals("IV")){
-                sum = sum + 4;
-                i = i+2;
-            }else if(val.equals("IX")){
-                sum = sum + 9;
-                i = i+2;
-            }else if(val.equals("XL")){
-                sum = sum + 40;
-                i = i+2;
-            }else if(val.equals("XC")){
-                sum = sum + 90;
-                i = i+2;
-            }else if(val.equals("CD")){
-                sum = sum + 400;
-                i = i+2;
-            }else if(val.equals("CM")){
-                sum = sum + 900;
-                i = i+2;
-            }else if(val.charAt(0) == 'I'){
-                sum = sum + 1;
-                i++;
-            }else if(val.charAt(0) == 'V'){
-                sum = sum + 5;
-                i++;
-            }else if(val.charAt(0) == 'X'){
-                sum = sum + 10;
-                i++;
-            }else if(val.charAt(0) == 'L'){
-                sum = sum + 50;
-                i++;
-            }else if(val.charAt(0) == 'C'){
-                sum = sum + 100;
-                i++;
-            }else if(val.charAt(0) == 'D'){
-                sum = sum + 500;
-                i++;
-            }else if(val.charAt(0) == 'M'){
-                sum = sum + 1000;
-                i++;
-            }
-
+            
+            if (num < prev) {
+                sum = sum - num;
+            } else sum = sum + num;
+             prev = num;
+            i--;
         }
-return sum;
+
+        return sum;
     }
 }
