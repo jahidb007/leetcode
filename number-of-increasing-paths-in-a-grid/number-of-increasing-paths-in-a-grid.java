e7ea779dbbfdd7219183087b8a4890a70ad7850f
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-06-18 23:03:08 +0600

    Time: 51 ms (60.43%) | Memory: 69.8 MB (8.63%) - LeetSync

--- number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.java
+++ number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.java
@@ -1,21 +1,21 @@
 class Solution {
     int mod = (int)(1e9+7);
      int[][] calculatedPath;
+     int[] result = new int[1];
     public int countPaths(int[][] grid) {
             int m = grid.length;
         int n = grid[0].length;
        calculatedPath = new int[m][n];
- int[] result = new int[1];
         for(int i = 0 ; i < m ; i++){
             for (int j = 0 ; j < n ; j++){
-                calculatePath(i,j,m,n,grid,result);
+                calculatePath(i,j,m,n,grid);
             }
         }
 
        return result[0] + (m * n);
     }
 
-   private  int calculatePath(int i, int j,int m, int n,int[][] grid,int[] result) {
+   private  int calculatePath(int i, int j,int m, int n,int[][] grid) {
         int totalPath = 0;
         if(i<0  || j < 0 || i >= m || j >= n ){
             return 0;
@@ -30,7 +30,7 @@ class Solution {
         if(j>0){
             if(grid[i][j-1] > grid[i][j]){
                 totalPath++;
-                totalPath = (totalPath + calculatePath(i,j-1,m,n,grid,result))%mod;
+                totalPath = (totalPath + calculatePath(i,j-1,m,n,grid))%mod;
             }
 
         }
@@ -40,7 +40,7 @@ class Solution {
         if(j<n-1){
             if(grid[i][j+1] > grid[i][j]){
                 totalPath++;
-                totalPath = (totalPath +  calculatePath(i,j+1,m,n,grid,result))%mod;
+                totalPath = (totalPath +  calculatePath(i,j+1,m,n,grid))%mod;
             }
         }
 
@@ -49,7 +49,7 @@ class Solution {
         if(i>0){
             if(grid[i-1][j] > grid[i][j]){
                 totalPath++;
-                totalPath = (totalPath +  calculatePath(i-1,j,m,n,grid,result))%mod;
+                totalPath = (totalPath +  calculatePath(i-1,j,m,n,grid))%mod;
             }
         }
 
@@ -58,7 +58,7 @@ class Solution {
         if(i<m-1){
             if(grid[i+1][j] > grid[i][j]){
                 totalPath++;
-                totalPath = (totalPath +  calculatePath(i+1,j,m,n,grid,result))%mod;
+                totalPath = (totalPath +  calculatePath(i+1,j,m,n,grid))%mod;
             }
         }
 
