789d1080b5ba7ba08b32be96d8772a6dc9af8340
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-08-29 21:45:19 +0600

    Time: 15 ms (42.09%) | Memory: 43.7 MB (97.64%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+         int[] a = new int[customers.length() + 1];
+        int count = 0;
+        for (int i = 0; i < customers.length(); i++) {
+            if (customers.charAt(i) == 'Y') {
+                count++;
+            }
+        }
+        a[0] = count;
+        int minHour = 0;
+        int minCount = count;
+
+        for (int i = 0; i < customers.length(); i++) {
+            if (i == 0) {
+                if (customers.charAt(0) == 'Y') {
+                    count--;
+                    a[i + 1] = count;
+                    if(count < minCount){
+                        minCount = count;
+                        minHour = i +1;
+                    }
+
+                    continue;
+                }
+            }
+
+            if (customers.charAt(i) == 'Y') {
+                count--;
+            }else count++;
+
+
+
+            a[i + 1] = count;
+            if(count < minCount){
+                minCount = count;
+                minHour = i +1;
+            }
+        }
+
+       return minHour;  
+    }
+}
