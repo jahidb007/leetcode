da5ce818b7837b809d73a1e4f2f784bbb8f0c8ac
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-09-11 07:15:14 +0600

    Time: 5 ms (93.77%) | Memory: 44.8 MB (9.06%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -1,9 +1,8 @@
 class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
         List<List<Integer>> groups = new ArrayList<>();
 
         Map<Integer,List<Integer>> buffer = new HashMap<>();
-    public List<List<Integer>> groupThePeople(int[] groupSizes) {
-    
 
         for(int i = 0 ; i < groupSizes.length ; i++){
 
