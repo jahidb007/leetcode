cb268b4761b8f2c993da00513cbce32bf24c2e9e
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-12-14 20:41:19 +0600

    Time: 21 ms (12.30%) | Memory: 71.9 MB (60.52%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -3,28 +3,26 @@ class Solution {
         int rl = grid.length;
         int cl = grid[0].length;
 
-        int[] rowOnes = new int[rl];
-        int[] rowZeros = new int[rl];
-        int[] colOnes = new int[cl];
-        int[] colZeros = new int[cl];
+        int[][] rowOnes = new int[rl][2];
+        int[][] colOnes = new int[cl][2];
 
         int[][] result = new int[rl][cl];
 
         for(int i = 0 ; i < rl ; i++){
             for(int j = 0; j < cl ; j++){
                 if(grid[i][j] == 1){
-                    rowOnes[i] += 1;
-                    colOnes[j] += 1;
+                    rowOnes[i][1] += 1;
+                    colOnes[j][1] += 1;
                 }else {
-                    rowZeros[i] += 1;
-                    colZeros[j] += 1;
+                    rowOnes[i][0] += 1;
+                    colOnes[j][0] += 1;
                 }
             }
         }
 
             for(int i = 0 ; i < rl ; i++){
                 for(int j = 0; j < cl ; j++){
-                    result[i][j] =  rowOnes[i] +  colOnes[j] - rowZeros[i] - colZeros[j] ;
+                    result[i][j] =  rowOnes[i][1] +  colOnes[j][1] - rowOnes[i][0] - colOnes[j][0] ;
                 }
             }
 
