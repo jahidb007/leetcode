507f89449684b5c3593449db4656ae9f84c14475
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-12-20 18:28:29 +0600

    Time: 1 ms (26.07%) | Memory: 44.5 MB (5.40%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -9,16 +9,19 @@ class Solution {
 
         int match = 0;
 
+        int lv = nums[0];
+
         for(int i = 1; i < nums.length; i++ ){
 
-            if( nums[i] != nums[i-1]){
-                nums[index] = nums[i];
-                index++;
-                match = 0;
-            }else if( match == 0){
+            if( lv == nums[i] && match == 0){
                 nums[index] = nums[i];
                 index++;
                 match++;
+            }else if( lv != nums[i]){
+                nums[index] = nums[i];
+                index++;
+                match = 0;
+                lv = nums[i];
             }
 
         }
