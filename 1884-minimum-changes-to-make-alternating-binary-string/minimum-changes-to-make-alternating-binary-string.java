fda190054ea77b4d69acf829ba916145123cb7ec
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-12-25 03:35:22 +0600

    Time: 447 ms (5.84%) | Memory: 45.3 MB (5.19%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -5,28 +5,32 @@ class Solution {
             return 0;
         }
         
-        int step0 = 0;
-        Character init = '0';
-
-        for(int i = 0 ; i < s.length() ; i++){
-                if(init != s.charAt(i)){
-                    step0++;
-                }
-                init = init == '0' ? '1' : '0';
+        Character prev = s.charAt(0);
+        int step = 0;
+        for(int i = 1; i < s.length(); i++){
+            Character next = s.charAt(i);
+            if(prev == next){
+                System.out.println(i + " "+prev );
+                step++;
+                prev = prev == '0' ? '1' : '0';
+                System.out.println(i + " "+prev );
+            }else prev = next;
         }
 
-        int step1 = 0;
-        init = '1';
-        for(int i = 0 ; i < s.length() ; i++){
-                if(init != s.charAt(i)){
-                    step1++;
-                }
-                init = init == '0' ? '1' : '0';
+        prev = s.charAt(0) == '0' ? '1' : '0';
+        int stepT = 1;
+        for(int i = 1; i < s.length(); i++){
+            Character next = s.charAt(i);
+            if(prev == next){
+                System.out.println(i + " "+prev );
+                stepT++;
+                prev = prev == '0' ? '1' : '0';
+                System.out.println(i + " "+prev );
+            }else prev = next;
         }
+        
 
-
-
-        return Math.min(step1,step0);
+        return Math.min(step,stepT);
         
     }
 }
