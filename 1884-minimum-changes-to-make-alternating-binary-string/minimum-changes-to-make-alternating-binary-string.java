e15ce1c494870963030e6032accd54f927f3fab5
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-12-25 03:44:37 +0600

    Time: 8 ms (7.47%) | Memory: 42.3 MB (9.09%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -1,19 +1,32 @@
 class Solution {
     public int minOperations(String s) {
-        char c = s.charAt(0);
-        int step1 = count(s, c);
-        int step2  = count(s,c == '0' ? '1' : '0') + 1;
-        return Math.min(step1,step2);
-    }
-    public int count(String s, char prev){
+
+        if(s.length() == 1){
+            return 0;
+        }
+        
+        Character prev = s.charAt(0);
         int step = 0;
         for(int i = 1; i < s.length(); i++){
-            char next = s.charAt(i);
+            Character next = s.charAt(i);
             if(prev == next){
                 step++;
                 prev = prev == '0' ? '1' : '0';
             }else prev = next;
         }
-        return step;
+
+        prev = s.charAt(0) == '0' ? '1' : '0';
+        int stepT = 1;
+        for(int i = 1; i < s.length(); i++){
+            Character next = s.charAt(i);
+            if(prev == next){
+                stepT++;
+                prev = prev == '0' ? '1' : '0';
+            }else prev = next;
+        }
+        
+
+        return Math.min(step,stepT);
+        
     }
 }
