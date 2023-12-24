74ffd5ed852ac5cff2f2c5291e66251dd66b5f15
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-12-25 01:46:29 +0600

    Time: 3 ms (8.83%) | Memory: 59.6 MB (5.21%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] dp = new int[nums.length];
+
+        if(nums.length == 1 || k == 0){
+            return;
+        }
+for(int i = 0 ; i < nums.length; i++){
+    if(dp[i] ==0) rotateRecur(nums,i, k, i,dp);
+       
+}
+
+
+        
+
+    }
+
+    public void rotateRecur(int[] nums,int ini,int k,int m,int[] dp){
+        int pos = ini + k;
+        if(pos > nums.length - 1 ){
+            pos = pos % nums.length;
+        }
+
+        if(pos == m){
+           return ;
+        }
+
+        rotateRecur(nums, pos, k,m,dp);
+        int temp = nums[ini];
+        nums[ini] = nums[pos];
+        nums[pos] = temp;
+        dp[ini] = -1;
+        dp[pos] = -1;
+    }
+}
+
+// 4 3 2 1  7 6 5
+// 5 6 7 1 2 3 4
+
+// 1 2 3 4 5 6 7
