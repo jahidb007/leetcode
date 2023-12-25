0100423d88bb24e4b08cf8c6421565b44643fb45
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-12-26 01:30:11 +0600

    Time: 0 ms (100.00%) | Memory: 41.7 MB (11.16%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,6 +1,7 @@
 class Solution {
     public int numDecodings(String s) {
     int[] dp = new int[s.length()];
+    Arrays.fill(dp, -1);
     return decode(s, dp, 0);
     }
     
@@ -12,7 +13,7 @@ class Solution {
             return 1;
         }
         
-        if (dp[i] != 0) {
+        if (dp[i] != -1) {
             return dp[i];
         }
 
