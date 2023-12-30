a1a36686adb66eb301b5f31c1e6f8966d5cce8d9
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2023-12-30 21:10:01 +0600

    Time: 3 ms (58.49%) | Memory: 43.9 MB (9.06%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
@@ -3,14 +3,17 @@ class Solution {
          if (words.length == 1) {
 			return true;
 		}
-        
-         int[] chr = new int[26];
+        int len = 0;
+        int[] chr = new int[26];
         for(String word : words){
 
             for(Character c : word.toCharArray()){
                 chr[c - 'a'] = chr[c - 'a'] + 1;
+                len++;
             }
         }
+
+        if(len % words.length !=0) return false;
 
         for(int i = 0 ; i < chr.length ; i++){
             if(chr[i] % words.length !=0 ){
