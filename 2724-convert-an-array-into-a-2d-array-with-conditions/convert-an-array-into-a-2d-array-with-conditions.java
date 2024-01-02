ffa9f42e777d462612ec9eae260a8651b741d88d
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2024-01-02 21:47:14 +0600

    Time: 1 ms (100.00%) | Memory: 45.1 MB (9.75%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -3,17 +3,16 @@ class Solution {
        int[] map = new int[nums.length + 1];
 
         List<List<Integer>> list = new ArrayList<>();
+        list.add(new ArrayList());
         
         for(int i = 0 ; i < nums.length; i++){
             
-            int j = map[nums[i]];
-            
-            if(list.size() <= j){
+            if(list.size() <= map[nums[i]]){
                 list.add(new ArrayList());
             }
 
-            list.get(j).add(nums[i]);
-            map[nums[i]] = j + 1;
+            list.get(map[nums[i]]).add(nums[i]);
+            map[nums[i]] = map[nums[i]] + 1;
         }
 
         return list;
