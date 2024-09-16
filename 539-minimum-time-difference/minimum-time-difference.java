7f33956b4940648b58d86d7ad8bf093c01b9f3ae
jahidb007 <45313465+jahidb007@users.noreply.github.com>
2024-09-16 23:59:59 +0600

    Time: 11 ms (36.10%) | Memory: 46.3 MB (17.48%) - LeetSync

---
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        int[] data = new int[timePoints.size()];
+
+        int i = 0;
+        for(String t : timePoints){
+            String hh = t.split(":")[0];
+            String mm = t.split(":")[1];
+            data[i] = Integer.valueOf(hh) * 60 + Integer.valueOf(mm);
+            i++;
+        }
+
+        Arrays.sort(data);
+        int min = 14444;
+        for( i = 1 ; i < data.length ; i++){
+            int diff = data[i] - data[i-1];
+          
+            
+            if(1439 - diff < diff){
+                diff = 1440 - diff;
+            }
+            
+            if(diff > 720){
+                diff = 720 - diff;
+            }
+
+            if(diff == 0 ){
+                return 0;
+            }
+
+            if(diff < min){
+                min = diff;
+            }
+
+        }
+
+
+       int diff = data[data.length - 1] - data[0];
+        if(1439 - diff < diff){
+                diff = 1440 - diff;
+            }
+            
+            if(diff > 720){
+                diff = 720 - diff;
+            }
+
+            if(diff < min){
+                min = diff;
+            }
+
+        return min;
+    }
+}
